51a24e0b79c254a10b835e823a4ed83ffede9d52
João Paulo <joaowais@gmail.com>
2023-07-18 14:22:22 -0300

    criado html e css desafio dia 2

--- 1 - Drum Kit/script.js
+++ 1 - Drum Kit/script.js
@@ -1,9 +1,8 @@
-
 function playSound(e) {
   const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
   const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
   if (!audio) return;
-  audio.currentTime = 0; //Volta áudio para o início
+  audio.currentTime = 0;
   audio.play();
   key.classList.add('tocar');
 }
@@ -14,5 +13,6 @@ function removeTransition(e) {
 }
 
 const keys = document.querySelectorAll('.key');
-keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+keys.forEach((key) => key.addEventListener('transitionend', removeTransition));
 window.addEventListener('keydown', playSound);
+
